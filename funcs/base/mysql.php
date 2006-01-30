4228904260b5a8a25367adb693159cb67261c5a8
Mark Karpeles <mark@hell.ne.jp>
2006-01-30 07:17:46 +0000

    Fixed (hopefully) mysql disconnection problems
    git-svn-id: https://ookoo.org/svn/pinetd@10 b5484250-a906-0410-b511-e608d867c28c

--- funcs/base/mysql.php
+++ funcs/base/mysql.php
@@ -1,6 +1,6 @@
 <?
   function getsql() {
     global $sql_user,$sql_pass,$sql_host;
-    return mysql_connect($sql_host,$sql_user,$sql_pass);
+    return mysql_connect($sql_host,$sql_user,$sql_pass, true); // new link !
   }
 ?>
