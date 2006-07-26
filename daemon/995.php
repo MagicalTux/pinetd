023f97d5b16ebf8da1973ce5a0617d2acbdf6618
Mark Karpeles <mark@hell.ne.jp>
2006-07-26 11:53:02 +0000

    git-svn-id: https://ookoo.org/svn/pinetd@62 b5484250-a906-0410-b511-e608d867c28c

---
+++ daemon/995.php
@@ -0,0 +1,14 @@
+<?php
+/* pmaild v2.0
+ * POP3 server rewrote from scratch (missing old sourcecode)
+ * $Id$
+ *
+ * This is just a redirection to the normal POP server, with a SSL layer
+ */
+
+if (!extension_loaded('openssl')) exit(180); // need SSL extension
+
+if (!defined('PINETD_SOCKET_TYPE')) define('PINETD_SOCKET_TYPE', 'ssl');
+
+require_once(dirname(__FILE__).'/110.php');
+
