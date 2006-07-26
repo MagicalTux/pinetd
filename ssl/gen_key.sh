adb374fbf91e5706251db5e96190f158cf951927
Mark Karpeles <mark@hell.ne.jp>
2006-07-26 12:48:55 +0000

    New version of gen_key.sh
    git-svn-id: https://ookoo.org/svn/pinetd@64 b5484250-a906-0410-b511-e608d867c28c

--- ssl/gen_key.sh
+++ ssl/gen_key.sh
@@ -4,14 +4,14 @@
 # Details found there :
 # http://sial.org/howto/openssl/self-signed/
 
+CERTIFICATE="newkey"
+ENCRYPT="no"
+
 OPENSSL=`which openssl 2>/dev/null`
 if [ x"$OPENSSL" = x ]; then
 	echo "Openssl needed. Please install it!"
 	exit 1
 fi
-
-openssl genrsa 1024 > newkey.key
-chmod 0400 newkey.key
 
 echo
 echo '*****'
@@ -19,8 +19,20 @@ echo "** When asked for Common Name, enter the server's name"
 echo '*****'
 echo
 
-openssl req -new -x509 -nodes -sha1 -days 365 -key newkey.key > newkey.cert
+"$OPENSSL" req -new -config ssl.cnf -out "$CERTIFICATE.req.pem" -keyout "$CERTIFICATE.key.pem" -nodes
+chmod 0400 newkey.key
 
-cat newkey.cert newkey.key >newkey.pem && rm -f newkey.key
-chmod 0400 newkey.pem
+"$OPENSSL" req -x509 -days 365 -in "$CERTIFICATE.req.pem" -key "$CERTIFICATE.key.pem" -out "$CERTIFICATE.cert.pem"
+"$OPENSSL" x509 -subject -dates -fingerprint -noout -in "$CERTIFICATE.cert.pem"
+
+chmod o-rwx "$CERTIFICATE.req.pem" "$CERTIFICATE.key.pem" "$CERTIFICATE.cert.pem"
+chmod g-rwx "$CERTIFICATE.req.pem" "$CERTIFICATE.key.pem" "$CERTIFICATE.cert.pem"
+
+if [ x"$ENCRYPT" != x"no" ]; then
+	"$OPENSSL" rsa -in "$CERTIFICATE.key.pem" -out "$CERTIFICATE.key.c.pem" -des3
+	if [ -f "$CERTIFICATE.key.c.pem" ]; then
+		cat >"$CERTIFICATE.key.pem" "$CERTIFICATE.key.c.pem"
+		rm -f "$CERTIFICATE.key.c.pem"
+	fi
+fi
 
