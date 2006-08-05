bbd970113aa3338b069ed56bbb658aae0856cf68
Mark Karpeles <mark@hell.ne.jp>
2006-08-05 17:02:34 +0000

    Added domainkey generation script
    git-svn-id: https://ookoo.org/svn/pinetd@85 b5484250-a906-0410-b511-e608d867c28c

---
+++ ssl/gen_local_domainkey.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# This file will generate a local SSL 768bits certificate for use in
+# local domains.
+
+OPENSSL=`which openssl 2>/dev/null`
+if [ x"$OPENSSL" = x ]; then
+	echo "Openssl needed. Please install it!"
+	exit 1
+fi
+
+# Generate key
+"$OPENSSL" genrsa -out domainkey.private 768
+
+# Get corresponding public key
+PUBLIC=`"$OPENSSL" rsa -in domainkey.private -pubout -outform PEM`
+
+# cleanup output to make it suitable for DNS records
+
+PUBLIC=`echo "$PUBLIC" | sed -e 's/-----.*-----//'`
+PUBLIC_OUT=""
+for foo in $PUBLIC; do PUBLIC_OUT="$PUBLIC_OUT$foo"; done
+
+echo
+echo "*****"
+echo "** Domain key ready. Add those lines to your DNS zones files"
+echo "** to enable it. Note that everyone will have to use this server"
+echo "** as a SMTP in order to have the mails accepted."
+echo "*****"
+echo "_domainkey.yourdomain.com.	IN	TXT	\"k=rsa\; t=y\; p=$PUBLIC_OUT\;\""
+
